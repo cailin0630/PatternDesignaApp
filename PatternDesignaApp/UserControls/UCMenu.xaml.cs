2e7f01add23e5881e60eafbd1d01ddd39d6664e3
alvinjiang <alvinjiang@yuantutech.com>
2017-08-07 22:12:48 +0800

    import

---
+++ PatternDesignaApp/UserControls/UCMenu.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace PatternDesignaApp.UserControls
+{
+    /// <summary>
+    /// Menu.xaml 的交互逻辑
+    /// </summary>
+    public partial class UCMenu : UserControl
+    {
+        public UCMenu()
+        {
+            InitializeComponent();
+        }
+    }
+}
