8b3b756150ea26dbb55030c818c9600381a62909
cailin0630 <903199067@qq.com>
2017-08-15 18:00:29 +0800

    update

---
+++ PatternDesignaApp/UserControls/UcMenu.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+namespace PatternDesignaApp.UserControls
+{
+    /// <summary>
+    /// Menu.xaml 的交互逻辑
+    /// </summary>
+    public partial class UcMenu : UserControl
+    {
+        public UcMenu()
+        {
+            InitializeComponent();
+
+
+        }
+    }
+}
